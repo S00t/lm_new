f682b8469d4856cf2aaa1a5cfb098a4d3d634c79
Grzegorz Głąbiński <Grzegorz.Glabinski@si-consulting.pl>
2017-05-16 15:06:08 +0200

    registration

--- app/src/main/java/soot/letsmeet/activities/register/controller/RegisterController.java
+++ app/src/main/java/soot/letsmeet/activities/register/controller/RegisterController.java
@@ -1,5 +1,8 @@
 package soot.letsmeet.activities.register.controller;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by Soot on 13/05/2017.
  */
@@ -12,9 +15,19 @@ public class RegisterController {
 
     }
 
+    public static boolean validateEmail(String email) {
+        return  email.matches(".+@.+\\.[a-z]+");
+    }
 
+    public static boolean validateSurname( String lastName ){
+        return lastName.matches("[a-zA-z]+([ '-][a-zA-Z]+)*");
+    }
 
-    private boolean checkNewPassRequirments(String newPasswd) {
+    private static boolean isTheSame(String password, String repeatPassword) {
+        return password.equals(repeatPassword);
+    }
+
+    private boolean checkPassRequirments(String newPasswd) {
         return newPasswd.matches("^(?=.*[a-z])(?=.*[A-Z])((?=.*\\d)|(?=.*[!\"#$%&'()*+,\\-./:;<=>?@\\[\\\\\\]^_`{|}~]))(?!.*[ \\t\\n\\x0B\\f\\r]).{6,}$");
     }
 
